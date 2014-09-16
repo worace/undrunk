b9a662b0014674e94deabba57d6d07248c7e9d7c
worace <horace.d.williams@gmail.com>
2014-09-15 21:51:40 -0400

    Initial Commit

---
+++ Undrunk/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Undrunk
+//
+//  Created by Horace Williams on 9/15/14.
+//  Copyright (c) 2014 WoracesWorkshop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "UDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([UDAppDelegate class]));
+    }
+}
